25beb5f69a35d23490e6ec564b2f7cec2c0e2ff9
Ik Hwan <dlrghks4444@gmail.com>
2020-10-15 15:27:26 +0900

    주문 상품 선택 화면

---
+++ RecyclerCardView/app/src/main/java/com/greenhelix/recyclercardview/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.greenhelix.recyclercardview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//https://dev-imaec.tistory.com/27
+//https://dev-imaec.tistory.com/28
+//https://dev-imaec.tistory.com/30
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
