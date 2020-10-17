522363f70236c696b53c9a426ef3db12d5691f93
Ik Hwan <dlrghks4444@gmail.com>
2020-10-17 18:19:01 +0900

    주문 형태 선택 ML 시작

--- Pear/app/src/main/java/com/greenhelix/pear/OrderKindActivity.java
+++ Pear/app/src/main/java/com/greenhelix/pear/OrderKindActivity.java
@@ -2,16 +2,27 @@ package com.greenhelix.pear;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OrderKindActivity extends AppCompatActivity {
 
+    Button btnOrderDirect;
+    Button btnOrderCamera;
+    Button btnOrderMain;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_order_kind);
+
+        btnOrderCamera = findViewById(R.id.btn_order_camera);
+        btnOrderDirect = findViewById(R.id.btn_order_direct);
+        btnOrderMain = findViewById(R.id.btn_select_order_before);
+
+
 
 
     }
