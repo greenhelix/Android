4ceccf671630909785b1a81da3de1c5d52d0b352
Ik Hwan <dlrghks4444@gmail.com>
2020-10-23 17:08:40 +0900

    Intent를 통한 이미지 비트맵 전달 완료 bitmap 형태의 파일 전달 방법 onActivityResult()사용법 숙지 필요.

--- Pear/app/src/main/java/com/greenhelix/pear/ImageML.java
+++ Pear/app/src/main/java/com/greenhelix/pear/ImageML.java
@@ -2,15 +2,34 @@ package com.greenhelix.pear;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class ImageML extends AppCompatActivity {
+//파이어베이스
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.util.Arrays;
+
+public class ImageML extends AppCompatActivity implements ImageAnalysis.Analyzer {
     private static final String BIT_IMAGE = "BitmapImage";
     private static final String LOG_TAG = "ik";
 
@@ -25,7 +44,79 @@ public class ImageML extends AppCompatActivity {
         Log.d(LOG_TAG, "비트맵이미지 수령 완료"+firebaseBitmapImage);
 
 
-    }
+        //가져온 비트맵 이미지를 파이어베이스 비전에 넣어준다.
+        FirebaseVisionImage test = FirebaseVisionImage.fromBitmap(firebaseBitmapImage);
+        // 파이어베이스 인식기를 선언해준다.
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                .getCloudTextRecognizer();
+        //파이어베이스 텍스트인식 옵션을 주는 곳이다. 여기서 한글인식을 설정하는듯
+        FirebaseVisionCloudTextRecognizerOptions txtOptions = new FirebaseVisionCloudTextRecognizerOptions.Builder()
+                .setLanguageHints(Arrays.asList("ko-KR","안녕"))
+                .build();
 
+        // 비트맵 이미지를 프로세스 돌리고 잘 작동하는지 확인.
+        Task<FirebaseVisionText> result = detector.processImage(test)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        Log.d(LOG_TAG,"텍스트인식기 정상가동");
+                        for(FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks()) {
+                            //그래픽으로 묶어주는 모습보여준는것?
+                            Rect boudingBox = block.getBoundingBox();
+                            Point[] cornerPoints = block.getCornerPoints();
+                            String text = block.getText();
+
+                            for (FirebaseVisionText.Line line : block.getLines()) {
+                                Log.d(LOG_TAG, "라인을 가져온다.");
+
+                                for (FirebaseVisionText.Element element : line.getElements()) {
+                                    Log.d(LOG_TAG, "각 단어들을 가져온다.");
+                                }//요소
+                            }//라인
+                        }//블록
+                    }//성공시
+                })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(LOG_TAG,"텍스트인식기 가동실패");
+                            }
+                        }
+                );
+        // 가이드에서는 바로 getText를 쓴다.
+//        String resultText = result.getResult().getText();
+//        for(FirebaseVisionText.TextBlock block: result.getResult().getTextBlocks())
+
+
+    }
+    private int degreesToFirebaseRotation(int degrees) {
+        switch (degrees) {
+            case 0:
+                return FirebaseVisionImageMetadata.ROTATION_0;
+            case 90:
+                return FirebaseVisionImageMetadata.ROTATION_90;
+            case 180:
+                return FirebaseVisionImageMetadata.ROTATION_180;
+            case 270:
+                return FirebaseVisionImageMetadata.ROTATION_270;
+            default:
+                throw new IllegalArgumentException(
+                        "Rotation must be 0, 90, 180, or 270.");
+        }
+    }
+    @Override
+    @androidx.camera.core.ExperimentalGetImage
+    public void analyze(ImageProxy image, int rotationDegrees) {
+        if (image == null || image.getImage() == null) {
+            return;
+        }
+        Image mediaImage = image.getImage();
+        int rotation = degreesToFirebaseRotation(rotationDegrees);
+        FirebaseVisionImage fbimage =
+                FirebaseVisionImage.fromMediaImage(mediaImage, rotation);
+        // Pass image to an ML Vision API
+        // ...
+    }
 }
 
