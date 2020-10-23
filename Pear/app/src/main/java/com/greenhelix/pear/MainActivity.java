dc5cbc1e44dbf9211ffd5663ecbd04af669e9863
Ik Hwan <dlrghks4444@gmail.com>
2020-10-24 02:50:57 +0900

    직접 배달 - 티맵 오픈 가능, 티맵 API 키를 발급받았고, 활용법을 알아봐야함. 하지만, 배송지 정보를 티맵으로 보내는 방법 찾아야함.

--- Pear/app/src/main/java/com/greenhelix/pear/MainActivity.java
+++ Pear/app/src/main/java/com/greenhelix/pear/MainActivity.java
@@ -3,6 +3,7 @@ package com.greenhelix.pear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.skt.Tmap.TMapTapi;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(LOG_TAG, "Mainactivity정상 oncreate되었습니다.");
+        Log.d(LOG_TAG, "Mainactivity oncreate 되었습니다.");
+
+        final TMapTapi tmaptapi = new TMapTapi(this);
+        tmaptapi.setSKTMapAuthentication ("l7xx67178473a0134850bb0610927c9ba539");
 
         //로그아웃
         btnMainLogout =findViewById(R.id.btn_main_logout);
@@ -37,23 +42,48 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                Log.d(LOG_TAG,"정상로그아웃 FirebaseAuth");
+                Log.d(LOG_TAG,"로그아웃 버튼이 정상적으로 눌렸습니다.");
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
                 finish();
             }
         });
 
         //주문입력
-        btnMainOrderDirect = findViewById(R.id.btn_main_order_write);
-        btnMainOrderDirect.setOnClickListener(new View.OnClickListener() {
+        btnMainOrderWrite = findViewById(R.id.btn_main_order_write);
+        btnMainOrderWrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(LOG_TAG, "버튼이 정상적으로 눌렸습니다.");
+                Log.d(LOG_TAG, "직접주문 버튼이 정상적으로 눌렸습니다.");
                 Intent account = new Intent(MainActivity.this, OrderKindActivity.class);
                 startActivity(account);
                 finish();
             }
         });
+
+        //직접배달
+        btnMainOrderDirect = findViewById(R.id.btn_main_direct);
+        btnMainOrderDirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "직접배달 버튼이 정상적으로 눌렸습니다.");
+                //티맵 패키지명을 가져와서, 인텐트를 통해 해당 앱을 켜준다.
+                try{
+                    Intent tmapOpen = getPackageManager().getLaunchIntentForPackage("com.skt.tmap.ku");
+                    tmapOpen.setAction(Intent.ACTION_SEND);
+                    tmapOpen.putExtra(Intent.EXTRA_TEXT, tmaptapi.invokeSearchPortal("혁규농원"));
+                    tmapOpen.setType("text/*");
+                    Intent shareIntent = Intent.createChooser(tmapOpen, null);
+                    startActivity(shareIntent);
+
+
+                }catch (Exception e){
+                    String url = "market://details?id=com.skt.tmap.ku";
+                    Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(i);
+                }
+            }
+        });
+
 
 
     }
