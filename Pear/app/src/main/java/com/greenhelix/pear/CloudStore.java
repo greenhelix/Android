4ba53ef8e4e90d8b6f02dc30a45fe84ea4d57e76
Ik Hwan <dlrghks4444@gmail.com>
2020-10-31 00:19:36 +0900

    Clout Store 확인하기 위한 테스트코드 작성.  document, collection 생성. 추가 확인 필요

--- Pear/app/src/main/java/com/greenhelix/pear/CloudStore.java
+++ Pear/app/src/main/java/com/greenhelix/pear/CloudStore.java
@@ -1,32 +1,42 @@
 package com.greenhelix.pear;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
 
 public class CloudStore extends AppCompatActivity {
 
     private static final String TEST = "ik_test";
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-//    @Override
-//    protected void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
-
-    //test
-    DocumentReference showOrder = db.collection("pear_orders").document("order1");
-
-
-
-
-
-
+    TextView show;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cloud_test);
+        show.findViewsWithText(R.id.cloudShow);
+    }
+    db.collection("pear_orders")
+            .get()
+        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        @Override
+        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Log.d(TAG, document.getId() + " => " + document.getData());
+                }
+            } else {
+                Log.w(TAG, "Error getting documents.", task.getException());
+            }
+        }
+    });
 }
