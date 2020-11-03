4fd90857b9a275de9e17df90fb5c4e83c914b142
Ik Hwan <dlrghks4444@gmail.com>
2020-11-04 00:21:33 +0900

    직접입력 edittext null지옥 통과 극협이였다. nullpointer, (형변환?) 예외처리가 정확하게 구분하고 가야겠다.
    Signed-off-by: Ik Hwan <dlrghks4444@gmail.com>

--- Pear/app/src/main/java/com/greenhelix/pear/test/DirectOrderActivity.java
+++ Pear/app/src/main/java/com/greenhelix/pear/test/DirectOrderActivity.java
@@ -2,17 +2,22 @@ package com.greenhelix.pear.test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.greenhelix.pear.R;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /*주문 직접 입력 화면*/
@@ -32,14 +37,13 @@ public class DirectOrderActivity extends AppCompatActivity {
         EditText address_detail2;
         Button direct_Next;
         Button direct_Before;
-        List<String> senderList;
-        List<String> recipientList;
+        List<String> senderList = new ArrayList<>();
+        List<String> recipientList= new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_direct_info);
-        Intent now = getIntent();
         direct_Next = (Button) findViewById(R.id.btn_direct_next);
         direct_Before = (Button) findViewById(R.id.btn_direct_before);
         direct_Before.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +54,7 @@ public class DirectOrderActivity extends AppCompatActivity {
         });
 
         try { //예외처리
-            sender_name.findViewById(R.id.et_direct_sender);
+
             sender_tel1.findViewById(R.id.et_direct_sender_tel1);
             sender_tel2.findViewById(R.id.et_direct_sender_tel2);
             sender_tel3.findViewById(R.id.et_direct_sender_tel3);
@@ -65,7 +69,18 @@ public class DirectOrderActivity extends AppCompatActivity {
             Log.d(LOG_TAG, "아무것도 안쓴상태"+e);
         }
         setResult(1);
+
+
     }//OnCreate
+    public void showData(View v){
+        sender_name = (EditText) findViewById(R.id.et_direct_sender);
+        senderList.add(sender_name.getText().toString());
+        Log.d(LOG_TAG,"show"+senderList);
+        Intent directData = new Intent(DirectOrderActivity.this, DirectShow.class);
+        directData.putExtra("sender", (Serializable) senderList);
+        directData.putExtra("recipient", (Serializable) recipientList);
+        startActivity(directData);
+    }
 
     @Override
     protected void onStart() {
@@ -73,8 +88,6 @@ public class DirectOrderActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "보내는 사람 리스트:: " +senderList);
         Log.d(LOG_TAG, "받는 사람 리스트:: " +recipientList);
     }
-
-
 
 
 }//class
--- Pear/app/src/main/java/com/greenhelix/pear/test/DirectShow.java
+++ Pear/app/src/main/java/com/greenhelix/pear/test/DirectShow.java
@@ -11,6 +11,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.greenhelix.pear.R;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class DirectShow extends AppCompatActivity {
@@ -25,8 +27,26 @@ public class DirectShow extends AppCompatActivity {
         setContentView(R.layout.test_show);
         Log.d(LOG_TAG,"DirectShow정상 가동되었습니다. OnCreate에 들어왔습니다.");
 
-        btnShow.findViewById(R.id.btn_click);
-        tvShow.findViewById(R.id.tv_test_show);
+        btnShow = (Button) findViewById(R.id.btn_click);
+        tvShow = (TextView) findViewById(R.id.tv_test_show);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> senderData = getIntent().getExtras().getStringArrayList("sender");
+                ArrayList<String> recipientData = getIntent().getExtras().getStringArrayList("recipient");
+                tvShow.append("보내는 사람 \n");
+                tvShow.append("보내는 사람 이름 : "+senderData.get(0)+"\n");
+//                tvShow.append("보내는 사람 전화번호 : "+senderData.get(1)+"-"+senderData.get(2)+"-"+senderData.get(3)+"\n");
+//
+//                tvShow.append("받는 사람 \n");
+//                tvShow.append("받는 사람 이름 :"+recipientData.get(0)+"\n");
+//                tvShow.append("받는 사람 전화번호 : "+recipientData.get(1)+"-"+recipientData.get(2)+"-"+recipientData.get(3)+"\n");
+//                tvShow.append("받는 사람 우편번호 :"+recipientData.get(4)+"\n");
+//                tvShow.append("받는 사람 주소 :"+recipientData.get(5)+"\n");
+//                tvShow.append("받는 사람 상세주소 :"+recipientData.get(6)+"\n");
+            }
+        });
     }
 
 }
