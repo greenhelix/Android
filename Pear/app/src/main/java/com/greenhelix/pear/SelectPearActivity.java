0f8584650fe3b3544fd9943f6849a76fa54d6b08
Ik Hwan <dlrghks4444@gmail.com>
2020-11-08 01:49:09 +0900

    배종류 선택 화면 활성화 / 최종 cloud 연동 작업 확인
    Signed-off-by: Ik Hwan <dlrghks4444@gmail.com>

--- Pear/app/src/main/java/com/greenhelix/pear/SelectPearActivity.java
+++ Pear/app/src/main/java/com/greenhelix/pear/SelectPearActivity.java
@@ -2,8 +2,10 @@ package com.greenhelix.pear;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,13 +19,30 @@ public class SelectPearActivity extends AppCompatActivity {
 
     Button pearKind1, pearKind2, pearKind3, pearKind4, pearKind5, pearKind6;
     Button pearAmount1 ,pearAmount2,pearAmount3,pearAmount4,pearAmount5,pearAmount6;
-    EditText pearBox;
+    NumberPicker pearBox;
     Button pearBefore, pearAfter;
+    Boolean btnSelect = true;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pear_select);
+        pearKind1 = findViewById(R.id.btn_pearKind1);
+        pearKind2 = findViewById(R.id.btn_pearKind2);
+        pearKind3 = findViewById(R.id.btn_pearKind3);
+        pearKind4 = findViewById(R.id.btn_pearKind4);
+        pearKind5 = findViewById(R.id.btn_pearKind5);
+        pearKind6 = findViewById(R.id.btn_pearKind6);
+        pearBox = findViewById(R.id.np_pearBox);
+        pearBox.setMaxValue(100);
+        pearBox.setMinValue(0);
+        pearBox.setOnValueChangedListener(new NumberPicker.OnValueChangeListener(){
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                String box = String.valueOf(newVal);
+
+            }
+        });
 
     }
 
