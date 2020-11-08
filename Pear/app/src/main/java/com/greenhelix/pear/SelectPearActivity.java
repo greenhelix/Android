8a0f59c548c9e584d0a9b646d9990bd80685516c
Ik Hwan <dlrghks4444@gmail.com>
2020-11-08 17:01:31 +0900

    배송상품 선택 화면 - 배종류, 배 갯수, 상자 갯수 활성화 완료.
    Signed-off-by: Ik Hwan <dlrghks4444@gmail.com>

--- Pear/app/src/main/java/com/greenhelix/pear/SelectPearActivity.java
+++ Pear/app/src/main/java/com/greenhelix/pear/SelectPearActivity.java
@@ -2,15 +2,22 @@ package com.greenhelix.pear;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelectPearActivity extends AppCompatActivity {
     /*배송 상품 선택 화면*/
@@ -21,7 +28,6 @@ public class SelectPearActivity extends AppCompatActivity {
     Button pearAmount1 ,pearAmount2,pearAmount3,pearAmount4,pearAmount5,pearAmount6;
     NumberPicker pearBox;
     Button pearBefore, pearAfter;
-    Boolean btnSelect = true;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,6 +39,12 @@ public class SelectPearActivity extends AppCompatActivity {
         pearKind4 = findViewById(R.id.btn_pearKind4);
         pearKind5 = findViewById(R.id.btn_pearKind5);
         pearKind6 = findViewById(R.id.btn_pearKind6);
+        pearAmount1 = findViewById(R.id.btn_pearAmount1);
+        pearAmount2 = findViewById(R.id.btn_pearAmount2);
+        pearAmount3 = findViewById(R.id.btn_pearAmount3);
+        pearAmount4 = findViewById(R.id.btn_pearAmount4);
+        pearAmount5 = findViewById(R.id.btn_pearAmount5);
+        pearAmount6 = findViewById(R.id.btn_pearAmount6);
         pearBox = findViewById(R.id.np_pearBox);
         pearBox.setMaxValue(100);
         pearBox.setMinValue(0);
@@ -44,6 +56,123 @@ public class SelectPearActivity extends AppCompatActivity {
             }
         });
 
+        pickPearKind(pearKind1);
+        pickPearKind(pearKind2);
+        pickPearKind(pearKind3);
+        pickPearKind(pearKind4);
+        pickPearKind(pearKind5);
+        pickPearKind(pearKind6);
+
+        pickPearAmount(pearAmount1);
+        pickPearAmount(pearAmount2);
+        pickPearAmount(pearAmount3);
+        pickPearAmount(pearAmount4);
+        pickPearAmount(pearAmount5);
+        pickPearAmount(pearAmount6);
     }
 
+    public void pickPearKind(final Button bt){
+            bt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), "클릭!",Toast.LENGTH_SHORT).show();
+                    Log.d(LOG_TAG, "버튼 선택....");
+                    if(bt == pearKind1){
+                        bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color));
+                        pearKind2.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind3.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind4.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind5.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind6.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                    }else if(bt == pearKind2){
+                        bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color));
+                        pearKind1.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind3.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind4.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind5.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind6.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                    }else if(bt == pearKind3){
+                        bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color));
+                        pearKind1.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind2.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind4.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind5.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind6.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                    }else if(bt == pearKind4){
+                        bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color));
+                        pearKind1.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind2.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind3.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind5.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind6.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                    }else if(bt == pearKind5){
+                        bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color));
+                        pearKind1.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind2.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind3.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind4.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind6.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                    }else if(bt == pearKind6) {
+                        bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color));
+                        pearKind1.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind2.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind3.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind4.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                        pearKind5.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.pear_btn_color_off));
+                    }
+                }
+            });
+    }
+    public void pickPearAmount(final Button bt){
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "클릭!",Toast.LENGTH_SHORT).show();
+                Log.d(LOG_TAG, "버튼 선택....");
+                if(bt == pearAmount1){
+                    bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color));
+                    pearAmount2.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount3.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount4.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount5.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount6.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                }else if(bt == pearAmount2){
+                    bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color));
+                    pearAmount1.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount3.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount4.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount5.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount6.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                }else if(bt == pearAmount3){
+                    bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color));
+                    pearAmount1.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount2.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount4.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount5.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount6.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                }else if(bt == pearAmount4){
+                    bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color));
+                    pearAmount1.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount2.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount3.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount5.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount6.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                }else if(bt == pearAmount5){
+                    bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color));
+                    pearAmount1.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount2.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount3.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount4.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount6.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                }else if(bt == pearAmount6) {
+                    bt.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color));
+                    pearAmount1.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount2.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount3.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount4.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                    pearAmount5.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.amount_btn_color_off));
+                }
+            }
+        });
+    }
 }
